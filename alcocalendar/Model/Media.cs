4d0ef22d84367eadd80c6e9e15752eafbf4c1d68
ollegoriy <oleggorin86@gmail.com>
2024-09-23 09:03:10 +0300

    Добавьте файлы проекта.

---
+++ alcocalendar/Model/Media.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alcocalendar.Model
+{
+    internal class Media
+    {
+        private SoundPlayer player;
+
+        public Media()
+        {
+            string exepath = AppDomain.CurrentDomain.BaseDirectory;
+            string path = Directory.GetParent(exepath)?.Parent?.Parent?.Parent?.FullName + "\\Media\\chapa.wav";
+            this.player = new SoundPlayer(path);
+        }
+
+        public void Play()
+        {
+            player.Play();
+        }
+
+        public void Stop()
+        {
+            player.Stop();
+        }
+    }
+}
